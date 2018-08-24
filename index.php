e5f62cd62e9f9e90e9743d467ae7f5b057b5b82f
QuentinDll <quentin.delplace13@gmail.com>
2018-08-24 10:00:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Exercice 2</title>
+    </head>
+    <body>
+        <form class="" action="user.php" method="post">
+            <label for="firstname">Nom: </label>
+            <input type="text" name="firstname" id="firstname" value="">
+            
+            <label for="lastname">Prénom: </label>
+            <input type="text" name="lastname" id="lastname" value="">
+            
+            <input type="button" value="Créer">
+        </form>
+    </body>
+</html>
